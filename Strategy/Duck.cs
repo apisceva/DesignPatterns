fa3cec2d32befe0d33b34f0091385553cb8dab4f
apisceva <aljona.gromova@gmail.com>
2022-07-01 06:10:48 -0400

    Base Class Duck

---
+++ Strategy/Duck.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    public abstract class Duck
+    {
+        public Duck()
+        {
+        }
+        public IFlyBehavior flyBehavior;
+        public IQuackBehavior quackBehavior;
+
+        public void PerformFly()
+        {
+            flyBehavior.Fly();
+        }
+        public void PerformQuack()
+        {
+            quackBehavior.DoQuack();
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine("All ducks float, even decoys!");
+        }
+    }
+
+}
