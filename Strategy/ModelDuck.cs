e482d76f47a081e2227ab54cdd4228f07ce0132f
apisceva <aljona.gromova@gmail.com>
2022-07-01 06:39:25 -0400

    new class Model Duck added

---
+++ Strategy/ModelDuck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    public class ModelDuck : Duck
+    {
+        public ModelDuck()
+        {
+            flyBehavior = new FlyNoWay();
+            quackBehavior = new Quack();
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("I'm a model duck.");
+        }
+    }
+}
