2685c15d9e4b21cef1d14c2d73a3853a7b590049
apisceva <aljona.gromova@gmail.com>
2022-07-01 06:31:34 -0400

    Duck Simulator added in Program

--- Strategy/Program.cs
+++ Strategy/Program.cs
@@ -1,4 +1,18 @@
-﻿public abstract class Duck
-{
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Strategy
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Duck mallard = new MallardDuck();
+            mallard.PerformQuack();
+            mallard.PerformFly();
+        }
+    }
 }
