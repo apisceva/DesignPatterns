bddf7d572134dd18f911f3556defc0e36c400a63
apisceva <aljona.gromova@gmail.com>
2022-07-01 06:13:03 -0400

    new class MallardDuck added

---
+++ Strategy/MallardDuck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    public class MallardDuck : Duck
+    {
+        public MallardDuck()
+        {
+            base.quackBehavior = new Quack();
+            base.flyBehavior = new FlyWithWings();
+        }
+
+        public void display()
+        {
+            Console.WriteLine("I'm a real Mallard duck.");
+        }
+    }
+}
